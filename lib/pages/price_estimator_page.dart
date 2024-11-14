9e3da7424ddc8614807b4142ac4b3e555a75cd75
lauraflores16 <laubel20flo@gmail.com>
2024-11-14 13:43:12 -0400

    estimacion-mejora

--- lib/pages/price_estimator_page.dart
+++ lib/pages/price_estimator_page.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
 
-// ya esta
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: PriceEstimatorPage(),
+    );
+  }
+}
 
 class PriceEstimatorPage extends StatefulWidget {
   @override
@@ -8,28 +19,18 @@ class PriceEstimatorPage extends StatefulWidget {
 }
 
 class _PriceEstimatorPageState extends State<PriceEstimatorPage> {
-  final _formKey = GlobalKey<FormState>();
-
-  String? originCity;
-  String? destinationCity;
-  double? weight;
+  String? selectedDimension;
+  int quantity = 1;
   double? length;
   double? height;
   double? width;
-  int quantity = 1;
-  double estimatedPrice = 0.0;
 
-  final List<String> boliviaDepartments = [
-    'La Paz',
-    'Cochabamba',
-    'Santa Cruz',
-    'Oruro',
-    'Potosí',
-    'Chuquisaca',
-    'Tarija',
-    'Beni',
-    'Pando',
-  ];
+  final Map<String, List<double>> predefinedDimensions = {
+    'Libro': [22, 16, 11],
+    'Par de zapatos': [33, 15, 16],
+    'Freidora o Mini Nevera': [50, 38, 42],
+    'Televisor 65"': [164, 100, 20],
+  };
 
   void adjustQuantity(bool increment) {
     setState(() {
@@ -41,152 +42,154 @@ class _PriceEstimatorPageState extends State<PriceEstimatorPage> {
     });
   }
 
-  void calculatePrice() {
-    if (_formKey.currentState?.validate() ?? false) {
-      setState(() {
-        // Fórmula simple de estimación de precio basada en peso y dimensiones
-        estimatedPrice = (weight ?? 1) * 2 +
-            (length ?? 0) * (width ?? 0) * (height ?? 0) * 0.001;
-      });
-    }
+  void selectPresetDimensions(String dimensionLabel) {
+    setState(() {
+      selectedDimension = dimensionLabel;
+      List<double> dimensions = predefinedDimensions[dimensionLabel]!;
+      length = dimensions[0];
+      width = dimensions[1];
+      height = dimensions[2];
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Estimador de Precios'),
+        title: Text('Estimador de Precio'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              DropdownButtonFormField<String>(
-                decoration: InputDecoration(labelText: 'Ciudad de Origen'),
-                value: originCity,
-                items: boliviaDepartments.map((department) {
-                  return DropdownMenuItem(
-                    value: department,
-                    child: Text(department),
-                  );
-                }).toList(),
-                onChanged: (value) {
-                  if (value != destinationCity) {
-                    setState(() {
-                      originCity = value;
-                    });
-                  } else {
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                      content: Text(
-                          'Ciudad de Origen y Ciudad de Destino deben ser diferentes.'),
-                    ));
-                  }
-                },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              '¿No estás seguro de las medidas?',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'Selecciona la medida de la caja que más se parezca al envío que quieres cotizar. Recuerda que son medidas de referencia.',
+              style: TextStyle(fontSize: 14, color: Colors.grey[600]),
+            ),
+            SizedBox(height: 16),
+            GridView.builder(
+              shrinkWrap: true,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 8,
+                crossAxisSpacing: 8,
+                childAspectRatio: 1.6,
               ),
-              DropdownButtonFormField<String>(
-                decoration: InputDecoration(labelText: 'Ciudad de Destino'),
-                value: destinationCity,
-                items: boliviaDepartments.map((department) {
-                  return DropdownMenuItem(
-                    value: department,
-                    child: Text(department),
-                  );
-                }).toList(),
-                onChanged: (value) {
-                  if (value != originCity) {
-                    setState(() {
-                      destinationCity = value;
-                    });
-                  } else {
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                      content: Text(
-                          'Ciudad de Origen y Ciudad de Destino deben ser diferentes.'),
-                    ));
-                  }
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Peso (kg)'),
-                keyboardType: TextInputType.number,
-                validator: (value) {
-                  if (value == null || value.isEmpty)
-                    return 'Por favor ingresa el peso';
-                  return null;
-                },
-                onChanged: (value) => weight = double.tryParse(value),
-              ),
-              TextFormField(
+              itemCount: predefinedDimensions.keys.length,
+              itemBuilder: (context, index) {
+                String dimensionLabel =
+                    predefinedDimensions.keys.elementAt(index);
+                List<double> dimensionValues =
+                    predefinedDimensions[dimensionLabel]!;
+                bool isSelected = selectedDimension == dimensionLabel;
+
+                return GestureDetector(
+                  onTap: () => selectPresetDimensions(dimensionLabel),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                          color: isSelected ? Colors.blue : Colors.grey),
+                      borderRadius: BorderRadius.circular(8),
+                      color: isSelected ? Colors.blue.shade100 : Colors.white,
+                    ),
+                    padding: EdgeInsets.all(8),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.inventory_2,
+                            size: 40, color: Colors.grey[700]),
+                        SizedBox(height: 8),
+                        Text(
+                          dimensionLabel,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black87,
+                          ),
+                        ),
+                        SizedBox(height: 4),
+                        Text(
+                          '${dimensionValues[0].toInt()} x ${dimensionValues[1].toInt()} x ${dimensionValues[2].toInt()}',
+                          style: TextStyle(
+                            color: Colors.grey[700],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+            SizedBox(height: 16),
+            if (selectedDimension != null) ...[
+              TextField(
                 decoration: InputDecoration(labelText: 'Largo (cm)'),
-                keyboardType: TextInputType.number,
-                validator: (value) {
-                  if (value == null || value.isEmpty)
-                    return 'Por favor ingresa el largo';
-                  return null;
-                },
-                onChanged: (value) => length = double.tryParse(value),
+                controller: TextEditingController(text: length?.toString()),
+                readOnly: true,
               ),
-              TextFormField(
+              TextField(
                 decoration: InputDecoration(labelText: 'Alto (cm)'),
-                keyboardType: TextInputType.number,
-                validator: (value) {
-                  if (value == null || value.isEmpty)
-                    return 'Por favor ingresa el alto';
-                  return null;
-                },
-                onChanged: (value) => height = double.tryParse(value),
+                controller: TextEditingController(text: height?.toString()),
+                readOnly: true,
               ),
-              TextFormField(
+              TextField(
                 decoration: InputDecoration(labelText: 'Ancho (cm)'),
-                keyboardType: TextInputType.number,
-                validator: (value) {
-                  if (value == null || value.isEmpty)
-                    return 'Por favor ingresa el ancho';
-                  return null;
-                },
-                onChanged: (value) => width = double.tryParse(value),
+                controller: TextEditingController(text: width?.toString()),
+                readOnly: true,
               ),
-              SizedBox(height: 16),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Cantidad',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  Row(
-                    children: [
-                      IconButton(
-                        onPressed: () => adjustQuantity(false),
-                        icon: Icon(Icons.remove),
-                      ),
-                      Text(
-                        '$quantity',
-                        style: TextStyle(fontSize: 18),
-                      ),
-                      IconButton(
-                        onPressed: () => adjustQuantity(true),
-                        icon: Icon(Icons.add),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: calculatePrice,
-                child: Text('Calcular Precio'),
-              ),
-              SizedBox(height: 16),
-              if (estimatedPrice > 0)
-                Text(
-                  'Precio estimado: Bs. ${estimatedPrice.toStringAsFixed(2)}',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                ),
             ],
-          ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Cantidad',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Row(
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.remove_circle_outline),
+                      onPressed: () => adjustQuantity(false),
+                    ),
+                    Text(
+                      quantity.toString(),
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.add_circle_outline),
+                      onPressed: () => adjustQuantity(true),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Spacer(),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text(
+                        'Cotización solicitada para $selectedDimension x $quantity'),
+                  ));
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.grey[300],
+                  foregroundColor: Colors.black54,
+                  padding: EdgeInsets.symmetric(vertical: 16),
+                ),
+                child: Text(
+                  'Solicitar Cotización',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
