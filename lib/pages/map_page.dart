f800426f27e2044d628c68bb7101046108de919f
DilanC ChN <2002dilanchoque@gmail.com>
2024-11-17 19:51:15 -0400

    maps

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,8 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
+  @override
+  _MapPageState createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  late GoogleMapController _mapController;
+
+  // Coordenadas iniciales del mapa
+  final LatLng _initialPosition = LatLng(-16.4897, -68.1193); // Ejemplo: La Paz, Bolivia
+
+  void _onMapCreated(GoogleMapController controller) {
+    _mapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Mapa'));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Mapa de Google'),
+      ),
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: _initialPosition,
+          zoom: 12.0,
+        ),
+        myLocationEnabled: true, // Mostrar botón de ubicación actual
+        myLocationButtonEnabled: true,
+        mapType: MapType.normal,
+      ),
+    );
   }
 }
