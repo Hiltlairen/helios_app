1a1a07cb3a973c8c93520ce37424fc5e497db66b
lauraflores16 <laubel20flo@gmail.com>
2024-11-19 00:31:50 -0400

    estimador terminado

--- lib/pages/price_estimator_page.dart
+++ lib/pages/price_estimator_page.dart
@@ -20,6 +20,7 @@ class PriceEstimatorPage extends StatefulWidget {
 
 class _PriceEstimatorPageState extends State<PriceEstimatorPage> {
   String? selectedProductType;
+  String? selectedPackageSubtype;
   String? originCity;
   String? destinationCity;
   double? weight;
@@ -39,6 +40,18 @@ class _PriceEstimatorPageState extends State<PriceEstimatorPage> {
     'Tarija',
     'Beni',
     'Pando',
+  ];
+
+  final List<String> packageSubtypes = [
+    'Frágil',
+    'Electrodomésticos',
+    'Ropa',
+    'Alimentos',
+    'Artículos de oficina',
+    'Herramientas',
+    'Tecnología',
+    'Muebles',
+    'Otros', // Añadido "Otros"
   ];
 
   double calculateQuotationInBolivianos() {
@@ -78,7 +91,8 @@ class _PriceEstimatorPageState extends State<PriceEstimatorPage> {
         weight == null ||
         quantity == null ||
         (selectedProductType == 'paquete' &&
-            (length == null || height == null || width == null))) {
+            (length == null || height == null || width == null)) ||
+        (selectedProductType == 'paquete' && selectedPackageSubtype == null)) {
       return false;
     }
     return true;
@@ -88,7 +102,7 @@ class _PriceEstimatorPageState extends State<PriceEstimatorPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blueAccent,
+        backgroundColor: const Color.fromARGB(255, 5, 32, 79),
         title:
             Text('Cotización de Envío', style: TextStyle(color: Colors.white)),
         centerTitle: true,
@@ -180,6 +194,34 @@ class _PriceEstimatorPageState extends State<PriceEstimatorPage> {
                 ),
               ),
             ),
+            if (selectedProductType == 'paquete')
+              Card(
+                elevation: 6.0,
+                margin: EdgeInsets.symmetric(vertical: 8.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      DropdownButtonFormField<String>(
+                        decoration:
+                            InputDecoration(labelText: 'Tipo de Paquete'),
+                        value: selectedPackageSubtype,
+                        items: packageSubtypes.map((subtype) {
+                          return DropdownMenuItem(
+                            value: subtype,
+                            child: Text(subtype),
+                          );
+                        }).toList(),
+                        onChanged: (value) {
+                          setState(() {
+                            selectedPackageSubtype = value;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             Card(
               elevation: 6.0,
               margin: EdgeInsets.symmetric(vertical: 8.0),
@@ -274,30 +316,62 @@ class _PriceEstimatorPageState extends State<PriceEstimatorPage> {
               keyboardType: TextInputType.number,
               onChanged: (value) => quantity = int.tryParse(value),
             ),
-            SizedBox(height: 16),
+            SizedBox(height: 20), // Aumentamos el espacio aquí
             Center(
               child: ElevatedButton(
                 onPressed: calculateQuotation,
-                child: Text('Calcular Cotización'),
+                child: Text('Cotizar envio'),
                 style: ElevatedButton.styleFrom(
-                  //primary: Colors.blueAccent, // Color de fondo
-                  padding: EdgeInsets.symmetric(horizontal: 32, vertical: 12),
-                  textStyle:
-                      TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  padding: EdgeInsets.symmetric(horizontal: 80, vertical: 20),
+                  //primary: Colors.transparent,
+                  //onPrimary: Colors.blueAccent,
+                  side: BorderSide(
+                      color: const Color.fromARGB(255, 12, 44, 98), width: 2),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                  ),
+                  //shadowColor: Colors.blueAccent.withOpacity(0.5),
+                  elevation: 8,
+                  textStyle: TextStyle(fontWeight: FontWeight.bold),
                 ),
               ),
             ),
             if (quotationInBolivianos != null)
-              Center(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 16.0),
-                  child: Text(
-                    'Cotización: Bs. ${quotationInBolivianos!.toStringAsFixed(2)}',
-                    style: TextStyle(
-                        fontSize: 18,
+              Container(
+                margin: EdgeInsets.only(top: 20),
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                  border: Border.all(color: Colors.black, width: 2),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.4),
+                      blurRadius: 8,
+                      offset: Offset(0, 4),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      'Cotización Estimada',
+                      style: TextStyle(
+                        fontSize: 20,
                         fontWeight: FontWeight.bold,
-                        color: Colors.blueAccent),
-                  ),
+                        color: Colors.black,
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      '${quotationInBolivianos!.toStringAsFixed(2)} Bs',
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
                 ),
               ),
           ],
